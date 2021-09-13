17d6005cd6cc2b9a7debe0f0fe9295c4099499d2
alex.zhang <gzane24@hotmail.com>
2021-09-14 00:10:34 +0800

    feat: add head menu

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { StoryBookApp } from '../src/widgets/StoryBookApp/StoryBookApp';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +9,11 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <StoryBookApp>
+      <Story />
+    </StoryBookApp>
+  ),
+];
